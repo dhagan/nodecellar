3595514c9df4abf265a648cba2f09a1768380174
Douglas Hagan <dhagan@yahoo.com>
2014-02-21 15:26:51 -1000

    allow cross domain

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.configure(function () {
     app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
     app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
 });
 
 app.get('/wines', wine.findAll);
